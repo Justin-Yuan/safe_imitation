02eb5df8b7d3b664b98bd6368a932411787307ba
Mustafa H <mhaid@cs.toronto.edu>
2021-08-25 23:48:07 -0400

    Add distance calculations, walls, and complete step()

--- safe_il/envs/simple_navigation.py
+++ safe_il/envs/simple_navigation.py
@@ -6,18 +6,13 @@ To demo the environment and visualize the randomized scene try:
 python safe_il/envs/simple_navigation.py
 """
 
-from Box2D.Box2D import b2CircleShape
+import math
+import Box2D
+from Box2D.Box2D import (
+    b2CircleShape, b2Distance, b2PolygonShape, b2Vec2)
+from gym.core import RewardWrapper
 import numpy as np
 
-import Box2D
-from Box2D.b2 import (
-    edgeShape,
-    circleShape,
-    fixtureDef,
-    polygonShape,
-    revoluteJointDef,
-    contactListener,
-)
 
 import gym
 from gym import spaces
@@ -31,9 +26,10 @@ STATE_H = 64
 STATE_W = 64
 VIEWPORT_W = 512
 VIEWPORT_H = 512
-FPS = 50
+FPS = 60
 SCALE = 30.0
 
+NUM_OBSTACLES = 3
 
 class SimpleNavigation(gym.Env, EzPickle):
     metadata = {"render.modes": ["human", "rgb_array"],
@@ -42,6 +38,8 @@ class SimpleNavigation(gym.Env, EzPickle):
     def __init__(self,
                  pixel_obs=False):
         self.name = 'Simple Navigation'
+        self.pixel_obs = pixel_obs
+        self.seed()
 
         # Action space is two floats, [horizontal force, vertical force]
         # Both forces act within the range -1 to +1
@@ -59,48 +57,103 @@ class SimpleNavigation(gym.Env, EzPickle):
 
         self.world = Box2D.b2World(gravity=(0, 0), doSleep=True)
         self.obstacles = []
-        self.goal_pos = (5, 5)
+        self.walls = []
+        self.agent_start_pos = (0, 0)
+        self.goal_pos = (0, 0)
         self.agent = None
         self.state = None
         self.done = None
         self.episode_steps = 0
         self.horizon = 500
 
+        # Reset during initialization
         self.reset()
 
     def step(self, action):
         action = np.clip(action, -1, +1).astype(np.float32).tolist()
         # Apply force to the agent
-        self.agent.ApplyForceToCenter(force=(action[0], action[1]), wake=True)
+        # TODO(mustafa): Should we apply noise?
+        self.agent.ApplyForceToCenter(force=b2Vec2(action), wake=True)
 
-        # Box2D World Simulation
+        # Box2D world simulation step (Next State)
         timeStep = 1.0 / 60
         vel_iters, pos_iters = 6, 2
         self.world.Step(timeStep, vel_iters, pos_iters)
 
-        reward = 0
         self.done = self.episode_steps >= self.horizon
-        state = [0]
+
+        # TODO(mustafa): Currently only returning vector-based obs,
+        # rgb_array obs will come from render function, might need to rewrite
+        # the step/render loop
+        state = []
+        distances = []
+
+        for obj in [self.goal] + self.obstacles:
+            distance = b2Distance(
+                shapeA=self.agent.fixtures[0].shape,
+                transformA=self.agent.fixtures[0].body.transform,
+                shapeB=obj.fixtures[0].shape,
+                transformB=obj.fixtures[0].body.transform
+                )
+            # Append the X and Y distance seperately
+            state.append(abs(distance.pointA[0] - distance.pointB[0]))
+            state.append(abs(distance.pointA[1] - distance.pointB[1]))
+            distances.append(distance.distance)
+
+        denom = math.hypot(
+            self.agent_start_pos[0] - self.goal_pos[0],
+            self.agent_start_pos[1] - self.goal_pos[1],
+            )
+        # Normalized reward based on distance from goal (and original distance)
+        # TODO(mustafa): This is a slightly wrong, as moving away will case
+        # distance > 1 so not actually normalized. Maybe we use max possible?
+        reward = distances[0] / denom
+
         return np.array(state, dtype=np.float32), reward, self.done, {}
 
-    def reset(self, random_start=False):
-        if random_start:
-            agent_start_pos = self.np_random.rand(2)
-        else:
-            agent_start_pos = (2, 2)
+    def reset(self, random_start=False, random_goal=False):
 
+        # Box2D memory management
+        self._destroy()
+
+        # Create and position agent
+        self.agent_start_pos = self.np_random.rand(2) if random_start else (2, 2)
         self.agent = self.world.CreateDynamicBody(
-            shapes=b2CircleShape(pos=agent_start_pos, radius=0.5)
-        )
-
+            shapes=b2CircleShape(pos=self.agent_start_pos, radius=0.5)
+            )
         self.agent.color = (1, 0, 0)  # Red
 
+        # Create and position goal
+        self.goal_pos = self.np_random.rand(2) if random_goal else (15, 15)
         self.goal = self.world.CreateStaticBody(
             shapes=b2CircleShape(pos=self.goal_pos, radius=0.5)
         )
         self.goal.color = (0, 1, 0)  # Green
 
-        self.drawlist = [self.agent, self.goal] + self.obstacles
+        # Generate the obstacles
+        for obstacle in range(NUM_OBSTACLES):
+            obs_pos = self.np_random.rand(2) * SCALE / 2
+            new_obstacle = self.world.CreateStaticBody(
+                shapes=b2CircleShape(pos=obs_pos, radius=0.5)
+                )
+            new_obstacle.color = (1, 1, 0.5)
+            self.obstacles.append(new_obstacle)
+
+        wall_size = [(18, 0.1), (0.1, 18), (18, 0.1), (0.1, 18)]
+        wall_pos = [(0, 0), (0, 0), (0, 17), (17, 0)]
+        for size, pos in zip(wall_size, wall_pos):
+            new_wall = self.world.CreateStaticBody(
+                shapes=b2PolygonShape(box=size),
+                position=pos
+            )
+            new_wall.color = (0, 0, 0)
+            self.walls.append(new_wall)
+
+        # TODO(mustafa): we might not need a drawlist, all bodies are stored
+        # in the world object, but just storing them all explicity for now
+
+        # Add all render objects to drawlist to loop over during
+        self.drawlist = [self.agent, self.goal] + self.obstacles + self.walls
 
         print('Environment has been reset!')
 
@@ -114,13 +167,14 @@ class SimpleNavigation(gym.Env, EzPickle):
         for obj in self.drawlist:
             for f in obj.fixtures:
                 trans = f.body.transform
-                if type(f.shape) is circleShape:
+                if type(f.shape) is b2CircleShape:
                     t = rendering.Transform(translation=trans * f.shape.pos)
                     self.viewer.draw_circle(
                         f.shape.radius, 20, color=obj.color
                     ).add_attr(t)
                 else:
-                    print('Not a circle')
+                    path = [trans * v for v in f.shape.vertices]
+                    self.viewer.draw_polygon(path, color=obj.color)
 
         return self.viewer.render(return_rgb_array=(mode == "rgb_aray"))
 
@@ -131,23 +185,34 @@ class SimpleNavigation(gym.Env, EzPickle):
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
+        return seed
 
     def _destroy(self):
         # Destroy all Box2D related entities to manage the memory efficiently
+        if not self.agent:
+            return
+
         self.world.DestroyBody(self.agent)
         self.world.DestroyBody(self.goal)
         for ob in self.obstacles:
             self.world.DestroyBody(ob)
 
+        for wall in self.walls:
+            self.world.DestroyBody(wall)
 
-def demo_simple_navigation(env, seed=None, render=False):
-    env.seed(seed)
+        self.obstacles = []
+        self.walls = []
+        self.agent = None
+        self.goal = None
+
+
+def demo_simple_navigation(env, render=False):
     total_reward = 0
 
     state = env.reset()
-    for _ in range(500):
+    for _ in range(1000):
         action = env.action_space.sample()
-        state, reward, done, info = env.step(action)
+        state, reward, done, info = env.step((action))
         total_reward += reward
 
         if render:
