05e098f15676adaaeca82f20fabcd80a3b616d5e
Mustafa H <mhaid@cs.toronto.edu>
2021-09-30 01:02:26 -0400

    Update config for plotting

--- safe_il/config.py
+++ safe_il/config.py
@@ -12,7 +12,7 @@ class AttrDict(dict):
 def get_base_parser():
     """Argument parser to load most basic training configuration"""
     parser = argparse.ArgumentParser(
-        description='Cutting Simulation with Graph Networks')
+        description='Description')
 
     # Util
     parser.add_argument('-c', '--config',
@@ -48,17 +48,46 @@ def get_base_parser():
                         default='model',
                         help='Name to use when saving model .pth file.')
 
+    parser.add_argument('--tag',
+                        type=str,
+                        default='basic',
+                        help='Name to use when saving model .pth file.')
+
+    parser.add_argument('--eval_interval',
+                        type=int,
+                        default=5,
+                        help='How many epochs before evaluating')
+
     # HyperParameters
-    parser.add_argument('--seed', type=int, default=52, help='Seed for RNG.')
+    parser.add_argument('--seed', type=int, default=523, help='Seed for RNG.')
 
     parser.add_argument('--epochs',
                         type=int,
-                        default=5,
+                        default=500,
                         help='Total number of epochs.')
 
     parser.add_argument('--batch_size',
                         type=int,
                         default=64,
                         help='Batch Size')
+
+    return parser
+
+
+def get_plot_parser():
+    """Argument parser to load model to plot visuals"""
+    parser = argparse.ArgumentParser(
+        description='Plotting')
+
+    parser.add_argument('--log_dir',
+                        type=str,
+                        default='logs/',
+                        required=True,
+                        help='Path to log directory.')
+
+    parser.add_argument('--filename',
+                        type=str,
+                        default='fig.png',
+                        help='Filename to save as')
 
     return parser
